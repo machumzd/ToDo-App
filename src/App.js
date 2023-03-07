33102fdf4e57fd1b0137ad39a29ac9eec5510ed0
machumzd <www.machuzz123@gmail.com>
2023-03-07 18:48:33 +0530

    delete,add,completed buttons added,date showed  day wise

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useState } from "react";
 function App() {
+  const [toDos, setToDos] = useState([]);
+  const [toDo, setToDo] = useState("");
+  const [currentDay] = useState(new Date().toLocaleString('en-US', { weekday: 'long' }));
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="mainHeading">
+        <h1>ToDo List</h1>
+      </div>
+      <div className="subHeading">
+        <br />
+        <h2>Whoop, it's {currentDay} 🌝 ☕ </h2>
+      </div>
+      <div className="input">
+        <input
+          value={toDo}
+          onChange={(e) => setToDo(e.target.value)}
+          type="text"
+          placeholder="🖊️ Add item..."
+        />
+        <i
+          onClick={() =>
+            setToDos([...toDos, { id: Date.now(), text: toDo, status: false }])
+          }
+          className="fas fa-plus"
+        ></i>
+      </div>
+      <div className="todos">
+        {toDos.map((obj) => {
+          return (
+            <div className="todo">
+              <div className="left">
+                <input
+                  onChange={(e) => {
+                    setToDos(
+                      toDos.filter((obj2) => {
+                        if (obj2.id === obj.id) {
+                          obj2.status = e.target.checked;
+                        }
+                        return obj2;
+                      })
+                    );
+                  }}
+                  value={obj.status}
+                  type="checkbox"
+                  name=""
+                  id=""
+                />
+                
+                <p>{obj.text}</p>
+              </div>
+              <div className="right">
+                <i onClick={(e)=>{
+                  setToDos(toDos.filter((obj2)=>{
+                    return obj2.id!==obj.id;
+                  }))
+                }
+              }
+                className="fas fa-times"></i>
+              </div>
+            </div>
+          );
+        })}
+        <h2 style={{marginTop:'2rem',borderBottom:"2px solid white"}}>Completed Tasks</h2>
+        {toDos.map((obj) => {
+          if (obj.status) {
+            return (
+              <div className="completed">
+                
+              <h4>{obj.text}</h4>
+              </div>
+            )
+
+          }
+          return null;
+        })}
+      </div>
     </div>
   );
 }
-
 export default App;
